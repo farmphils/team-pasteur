6e6a05e7f1fe187677e0a50a1671c144346afda3
Rohit Satyam <rohitsatyam102@gmail.com>
2021-10-10 12:17:59 -0700

    Rohit Adding Hello Script

---
+++ helloRohit.R
@@ -0,0 +1,7 @@
+myDetails <- function(name,email,discordid){
+	  cat(paste("Welcome!!",name,"\n"))
+  cat(paste("Your Email ID is",email,"\n"))
+    cat(paste("Your Discord ID is",discordid,"\n"))
+    }
+
+myDetails("Rohit Satyam","rohitsatyam102@gmail.com","rohitsatyam")
