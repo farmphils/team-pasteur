31af8489bcb8fedeaa80f4c473b61065f44540b6
iamjamesuche <ogbarized@yahoo.com>
2021-10-11 14:34:17 +0100

    Create iamjamesucheStage_0.R

---
+++ iamjamesucheStage_0.R
@@ -0,0 +1,7 @@
+name <- "James Uche"
+email <- "uchej@student.babcock.edu.ng"
+discord_username <- "@iamjamesuche#7502"
+biostack <- "Data Science"
+cat (name,email,discord_username,biostack,sep=", ")
+
+cat (sep=" \n")
