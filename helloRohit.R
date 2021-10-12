6019758b5d9748a23bad51d116ce30341fbcffa7
Rohit Satyam <rohitsatyam102@gmail.com>
2021-10-12 03:40:11 -0700

    Update helloRohit.R
    added biostack

--- helloRohit.R
+++ helloRohit.R
@@ -1,7 +1,8 @@
-myDetails <- function(name,email,discordid){
+myDetails <- function(name,email,discordid,biostack){
 	  cat(paste("Welcome!!",name,"\n"))
   cat(paste("Your Email ID is",email,"\n"))
     cat(paste("Your Discord ID is",discordid,"\n"))
+	cat(paste("Your Biostack is",biostack))
     }
 
-myDetails("Rohit Satyam","rohitsatyam102@gmail.com","rohitsatyam")
+myDetails("Rohit Satyam","rohitsatyam102@gmail.com","rohitsatyam","Machine Learning and Data Science")
