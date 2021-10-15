8770ee62ccb02dc79c2c06c6578eec941931be70
iamjamesuche <88292141+iamjamesuche@users.noreply.github.com>
2021-10-15 23:36:26 +0100

    Add files via upload

--- iamjamesucheStage_0.R
+++ iamjamesucheStage_0.R
@@ -1,7 +1,7 @@
 name <- "James Uche"
 email <- "jamezist@gmail.com"
-discord_username <- "@iamjamesuche#7502"
-biostack <- "Data Science"
-cat (name,email,discord_username,biostack,sep=", ")
+slack_username <- "@iamjamesuche"
+biostack <- "Machine Learning & Data Science"
+cat (name,email,slack_username,biostack,sep=", ")
 
 cat (sep=" \n")
