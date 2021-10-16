2c333fe13344d733c28a5150e0f30694f2e608c7
iamjamesuche <88292141+iamjamesuche@users.noreply.github.com>
2021-10-16 09:51:13 +0100

    Delete helloRohit.R

--- helloRohit.R
+++
@@ -1,8 +0,0 @@
-myDetails <- function(name,email,discordid,biostack){
-	  cat(paste("Welcome!!",name,"\n"))
-  cat(paste("Your Email ID is",email,"\n"))
-    cat(paste("Your Discord ID is",discordid,"\n"))
-	cat(paste("Your Biostack is",biostack))
-    }
-
-myDetails("Rohit Satyam","rohitsatyam102@gmail.com","rohitsatyam","Machine Learning and Data Science")
