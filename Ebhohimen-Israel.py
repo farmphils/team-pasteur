679bab9f9c62d21afe1d3ae8f951380c3ab4e87c
Ebhohimen Israel <71639669+Ebhohimen-Israel@users.noreply.github.com>
2021-10-16 13:35:39 +0100

    Create Ebhohimen-Israel.py

---
+++ Ebhohimen-Israel.py
@@ -0,0 +1,3 @@
+name, email, slack_ID, = "israelebhohimen", "israel.ebhohimen@gmail.com", "@Aizy85"
+Biostack = "Drug Discovery"
+print("Name: {}\nEmail: {}\nslack_ID: {}\nBiostack: {}".format(name, email, slack_ID, Biostack))
