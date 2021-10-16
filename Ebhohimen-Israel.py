6abe6674fab25712f214dcb261f102664370e0c4
Ebhohimen Israel <71639669+Ebhohimen-Israel@users.noreply.github.com>
2021-10-16 13:58:40 +0100

    Delete Ebhohimen-Israel.py

--- Ebhohimen-Israel.py
+++
@@ -1,3 +0,0 @@
-name, email, slack_ID, = "israelebhohimen", "israel.ebhohimen@gmail.com", "@Aizy85"
-Biostack = "Drug Discovery"
-print("Name: {}\nEmail: {}\nslack_ID: {}\nBiostack: {}".format(name, email, slack_ID, Biostack))
