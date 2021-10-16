6af99e6cf71008472878152374dc260547a88f55
ShubhangiChakraborty <92271396+ShubhangiChakraborty@users.noreply.github.com>
2021-10-16 13:17:03 +0530

    Add files via upload

---
+++ Shubhangi hackbio task1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+
+name= "Shubhangi Chakraborty"
+email= "chakraborty.shubhangi@gmail.com"
+Slack="Shubhangi-chakraborty"
+Biostack1="Vaccine Informatics"
+print(name,",",email,",",Slack,",",Biostack1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
